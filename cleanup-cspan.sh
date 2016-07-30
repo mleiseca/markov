a209592136ffe0555c2e5c5daf23216819fe0cfc
Michael Leiseca <mleiseca@gmail.com>
2016-07-30 10:30:49 +0200

    creating usable sed script

--- cleanup-cspan.sh
+++ cleanup-cspan.sh
@@ -1,69 +1,73 @@
-sed -i
- -e "s/president of the united states of america/President of the United States of America/g" \
- -e "s/united states of america/United States of America/g" \
- -e "s/united states congress/United States Congress/g" \
- -e "s/president barack obama/President Barack Obama/g" \
- -e "s/united states capitol/United States Capitol/g" \
- -e "s/vice president biden/Vice President Biden/g" \
- -e "s/united states senate/United States Senate/g" \
- -e "s/secretary of state/Secretary of State/g" \
- -e "s/planned parenthood/Planned Parenthood/g" \
- -e "s/independence hall/Independence Hall/g" \
- -e "s/governor o'malley/Governor O'malley/g" \
- -e "s/the liberty bell/the Liberty Bell/g" \
- -e "s/trump university/Trump University/g" \
- -e "s/elizabeth warren/Elizabeth Warren/g" \
- -e "s/african-american/African-American/g" \
- -e "s/the president's/the President's/g" \
- -e "s/president obama/President Obama/g" \
- -e "s/north carolina/North Carolina/g" \
- -e "s/know nothings/Know Nothings/g" \
- -e "s/latin america/Latin America/g" \
- -e "s/philadelphia/Philadelphia/g" \
- -e "s/constitution/Constitution/g" \
- -e "s/new hampshire/New Hampshire/g" \
- -e "s/rhode island/Rhode Island/g" \
- -e "s/bill clinton/Bill Clinton/g" \
- -e "s/new yorkers/New Yorkers/g" \
- -e "s/white house/White House/g" \
- -e "s/connecticut/Connecticut/g" \
- -e "s/trump tower/Trump Tower/g" \
- -e "s/mississippi/Mississippi/g" \
- -e "s/white house/White House/g" \
- -e "s/puerto rico/Puerto Rico/g" \
- -e "s/democratic/Democratic/g" \
- -e "s/republican/Republican/g" \
- -e "s/mike pence/Mike Pence/g" \
- -e "s/california/California/g" \
- -e "s/first lady/First Lady/g" \
- -e "s/mayflower/Mayflower/g" \
- -e "s/americans/Americans/g" \
- -e "s/democrats/Democrats/g" \
- -e "s/minnesota/Minnesota/g" \
- -e "s/tim kaine/Tim Kaine/g" \
- -e "s/september/September/" \
- -e "s/american/American/g" \
- -e "s/maryland/Maryland/g" \
- -e "s/november/November/g" \
- -e "s/congress/Congress/g" \
- -e "s/new york/New York/g" \
- -e "s/sanders/Sanders/g" \
- -e "s/clinton/Clinton/g" \
- -e "s/hillary/Hillary/g" \
- -e "s/america/America/g" \
- -e "s/indiana/Indiana/g" \
- -e "s/mexican/Mexican/g" \
- -e "s/montana/Montana/g" \
- -e "s/africa /Africa/g" \
- -e "s/rodham/Rodham/g" \
- -e "s/bernie/Bernie/g" \
- -e "s/muslim/Muslim/g" \
- -e "s/friday/Friday/g" \
- -e "s/oregon/Oregon/g" \
- -e "s/europe/Europe/g" \
- -e "s/mexico/Mexico/g" \
- -e "s/trump/Trump/g" \
- -e "s/lgbtq/LGBTQ/g" \
- -e "s/asia/Asia/g" \
- -e "s/god/God/g" \
- -e "s/u.s./US/g"
+s/president of the united states of america/President of the United States of America/g
+s/united states of america/United States of America/g
+s/united states congress/United States Congress/g
+s/president barack obama/President Barack Obama/g
+s/united states capitol/United States Capitol/g
+s/vice president biden/Vice President Biden/g
+s/united states senate/United States Senate/g
+s/secretary of state/Secretary of State/g
+s/planned parenthood/Planned Parenthood/g
+s/independence hall/Independence Hall/g
+s/governor o'malley/Governor O'malley/g
+s/the liberty bell/the Liberty Bell/g
+s/trump university/Trump University/g
+s/elizabeth warren/Elizabeth Warren/g
+s/african-american/African-American/g
+s/the president's/the President's/g
+s/president obama/President Obama/g
+s/north carolina/North Carolina/g
+s/know nothings/Know Nothings/g
+s/latin america/Latin America/g
+s/philadelphia/Philadelphia/g
+s/constitution/Constitution/g
+s/new hampshire/New Hampshire/g
+s/rhode island/Rhode Island/g
+s/bill clinton/Bill Clinton/g
+s/new yorkers/New Yorkers/g
+s/white house/White House/g
+s/connecticut/Connecticut/g
+s/trump tower/Trump Tower/g
+s/mississippi/Mississippi/g
+s/white house/White House/g
+s/puerto rico/Puerto Rico/g
+s/democratic/Democratic/g
+s/republican/Republican/g
+s/mike pence/Mike Pence/g
+s/california/California/g
+s/first lady/First Lady/g
+s/mayflower/Mayflower/g
+s/americans/Americans/g
+s/president/President/g
+s/senator/Senator/g
+s/democrats/Democrats/g
+s/democrat/Democrat/g
+s/minnesota/Minnesota/g
+s/tim kaine/Tim Kaine/g
+s/september/September/
+s/american/American/g
+s/maryland/Maryland/g
+s/november/November/g
+s/congress/Congress/g
+s/new york/New York/g
+s/sanders/Sanders/g
+s/clinton/Clinton/g
+s/donald/Donald/g
+s/hillary/Hillary/g
+s/america/America/g
+s/indiana/Indiana/g
+s/mexican/Mexican/g
+s/montana/Montana/g
+s/africa /Africa/g
+s/rodham/Rodham/g
+s/bernie/Bernie/g
+s/muslim/Muslim/g
+s/friday/Friday/g
+s/oregon/Oregon/g
+s/europe/Europe/g
+s/mexico/Mexico/g
+s/trump/Trump/g
+s/lgbtq/LGBTQ/g
+s/obama/Obama/g
+s/asia/Asia/g
+s/god/God/g
+s/ i / I /g
